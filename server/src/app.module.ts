ac5a55bab8bc0cc589f5d02520c631332eb13a49
cowboboy <r2d2tv@yandex.ru>
2024-01-20 14:37:40 +0500

    init config module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -4,9 +4,15 @@ import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { TaskModule } from './task/task.module';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UserModule, TaskModule, AuthModule],
+  imports: [
+    UserModule, 
+    TaskModule, 
+    AuthModule, 
+    ConfigModule.forRoot(),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
